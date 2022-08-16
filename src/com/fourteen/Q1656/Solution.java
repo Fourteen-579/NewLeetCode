cacea8abddcee5f7a6f04e0117a1525a80220361
kushuzhen <406499401@qq.com>
2022-08-16 16:17:43 +0800

    8.16号完成啦~

--- src/com/fourteen/Q1656/Solution.java
+++ src/com/fourteen/Q1656/Solution.java
@@ -1,5 +1,8 @@
 package com.fourteen.Q1656;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -8,15 +11,34 @@ import java.util.List;
  * @date 2022/8/16 9:12
  */
 public class Solution {
+    public static void main(String[] args) {
+        String[] attay = new String[5];
+        attay[3] = "10";
+        System.out.println(Arrays.toString(attay));
+    }
 }
 
 class OrderedStream {
 
-    public OrderedStream(int n) {
+    String[] array = null;
+    int index = 0;
 
+    public OrderedStream(int n) {
+        array = new String[n];
     }
 
     public List<String> insert(int idKey, String value) {
-        return null;
+        List<String> result = new ArrayList<>();
+
+        array[idKey - 1] = value;
+
+        if (idKey == index + 1) {
+            while (index < array.length && array[index] != null) {
+                result.add(array[index]);
+                index++;
+            }
+        }
+
+        return result;
     }
 }
