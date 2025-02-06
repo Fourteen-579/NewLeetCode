818bd512af73daae2134559e9d15391b970165c5
库淑贞 <kushuzhen@eastmoney.com>
2025-02-06 09:08:55 +0800

    练习

---
+++ src/com/fourteen/Q2972/Solution.java
@@ -0,0 +1,17 @@
+package com.fourteen.Q2972;
+
+/**
+ * @author Fourteen_ksz
+ * @version 1.0
+ * @date 2024/7/11 14:51
+ */
+public class Solution {
+
+    public long incremovableSubarrayCount(int[] nums) {
+        int result = 0;
+
+
+        return result;
+    }
+
+}
